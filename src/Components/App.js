901a82eea2bf747e15ccd218a3738713a1336b00
CarlEverest78 <thepythoneer@gmail.com>
2022-08-30 19:50:01 +0200

    first commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,9 @@ import React from  "react"
 import firebase from "firebase/app"
 import "firestore/firestore"
 import "firebase/auth"
-
 import {useAuthState} from "react-firebase-hooks/auth"
 import {useCollectionData} from "react-firebase-hooks/firestore"
+import SignIn from "./SignIn"
 
 firebase.initializeApp({
   apiKey: "AIzaSyCRcGwwxufrDebkMmbyKORG_XmUOVXgnGM",
